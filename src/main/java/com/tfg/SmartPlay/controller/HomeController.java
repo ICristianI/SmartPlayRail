e0d2df1feff38c3b31d454db7689e7a3183c1e42
Cristian <cristianyanez999@gmail.com>
2025-05-28 23:31:22 +0200

    .

--- src/main/java/com/tfg/SmartPlay/controller/HomeController.java
+++ src/main/java/com/tfg/SmartPlay/controller/HomeController.java
@@ -7,11 +7,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 // Controlador para manejar las rutas de la aplicación generales y que no requieren acciones adicionales o específicas.
 
 @Controller
 public class HomeController {
+
+    @RestController
+class HealthCheckController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
+
 
     
     // lleva a pagina de inicio
